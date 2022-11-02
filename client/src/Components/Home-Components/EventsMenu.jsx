5de49119fa329108af7ca3473bc266b6508fd72a
Patriciopg02 <105330418+Patriciopg02@users.noreply.github.com>
2022-11-02 18:49:26 -0300

    Update EventsMenu.jsx

--- client/src/Components/Home-Components/EventsMenu.jsx
+++ client/src/Components/Home-Components/EventsMenu.jsx
@@ -6,7 +6,7 @@ import './EventsMenu.css'
 export default function EventsMenu() {
     return (
 
-        window.location.href === `http://localhost:3000/home` ? (
+        window.location.href === `https://concatus.vercel.app/home` ? (
             <Card sx={{width:200, bgcolor:'custom.dark', borderRadius:3}}>
             <div className="eventsMenu">
                 <Typography sx={{fontFamily: 'Nunito', fontSize: 14,color:'primary.main'}} gutterBottom variant="h5" component="div">
